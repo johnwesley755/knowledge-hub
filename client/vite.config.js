9a4239727082c84db93de9303e5c1f2f2d0b82c0
johnwesley755 <johnwesley8113@gmail.com>
2025-08-31 16:39:51 +0530

    msg

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://knowledge-hub-kk06.onrender.com",
+        target: "https://knowledge-hub-kk06.onrender.com/",
         changeOrigin: true,
       },
     },
