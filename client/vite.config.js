3b271c8f4aeeee733384bbf095a35e8a69719694
johnwesley755 <johnwesley8113@gmail.com>
2025-08-31 18:56:35 +0530

    msg

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/", // 👈 ensures correct base path for Vercel
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://knowledge-hub-kk06.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
-        // 👇 Add this line to remove the /api prefix
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
